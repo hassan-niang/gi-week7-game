7b26733b055695bf90ad4930a9cae1c903cbc1fb
hassan-niang <hassanniang03@gmail.com>
2023-10-29 16:39:10 -0400

    final sumbit

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
-
-// function rounds(playerChoice, compChoiceFinal){
-    //     if (playerChoice === compchoice){
-        //        return message(`Tie Round!`);
-        //     } else if (playerChoice == rock && compChoiceFinal == scissors){
-            //         return messages[1];
-            //     } else if (playerChoice == scissors && compChoiceFinal == paper){
-                //         return messages[1];
-                //     }else if (playerChoice == paper && compChoiceFinal == rock){
-                    //         return messages[1];
-                    //     } else {
-                        //         return messages[0];
-                        //     }
-                        
-                        // }
-                 
-                        
-                        let choices = [`rock`, `paper`,`scissors`]
-                        
-                        // game = (playerChoice,compChoice) => {                                        
-                            //     let messages = [`White Walkers have advanced onto the wall!`, `You've pushed the White Walkers Back!`, `The Night King has taken over Westrios! Run for your life !`, `Westrios is saved! You have defeated the White Walkers!`, `You've Tied No One Has Advanced`];
-                            //     if (playerChoice === compChoice){
-                                //                return messages[4];
-                                //             } else if (playerChoice == 'rock' && compChoice == 'scissors'){
-                                    //                 return messages[1];
-                                    //             } else if (playerChoice == 'scissors' && compChoice == 'paper'){
-                                        //                 return messages[1];
-    //             } else if (playerChoice == 'paper' && compChoice == 'rock'){
-        //                 return messages[1];
-        //             } else {
-            //                 return messages[0];
-            //             }    
-            
-            //         }     
 const playerbtn = document.querySelectorAll('[data-selection]')                      
 const finalColumn = document.querySelector(`[data-final-column]`)
+const compScoreSpan = document.querySelector(`[data-computer-score]`)
+const yourScoreSpan = document.querySelector(`[data-your-score]`)
 
 const playerChoice = [
     {
@@ -67,8 +35,13 @@ function makeSelection(selection) {
     
     addSelectionResults(compSelction,compWinner)
     addSelectionResults(selection,yourWinner)
-
-    }
+    
+    if (yourWinner) incrementScore (yourScoreSpan)
+    if (compWinner) incrementScore (compScoreSpan)
+    } 
+function incrementScore (scoreSpan){
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
 function addSelectionResults (selection, winner){
     const div = document.createElement('div')
     div.innerText = selection.emoji
